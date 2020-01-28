438e5b707541c4b346caaa946bc76d2951790e9d
liu Wenxuan <woshitc8535@163.com>
2020-01-27 22:17:55 -0500

    Set up Layout of Login and Register

--- app/src/main/java/com/laioffer/usedbook/LoginActivity.java
+++ app/src/main/java/com/laioffer/usedbook/LoginActivity.java
@@ -3,12 +3,32 @@ package com.laioffer.usedbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button login;
+    private TextView toRegister;
+
+    private TextView forgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+
+
+    private void init() {
+        mEmail = findViewById(R.id.text_mail);
+        mPassword = findViewById(R.id.text_password);
+        login = findViewById(R.id.btn_login);
+        toRegister = findViewById(R.id.toRegister);
+        forgot = findViewById(R.id.text_forgot);
     }
 }
--- app/src/main/java/com/laioffer/usedbook/RegisterActivity.java
+++ app/src/main/java/com/laioffer/usedbook/RegisterActivity.java
@@ -1,14 +1,60 @@
 package com.laioffer.usedbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText mUsername;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button register;
+    private TextView toLogin;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        init();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Register");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsername.getText().toString();
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+            }
+        });
+    }
+
+
+
+
+
+
+
+    private void init() {
+        mUsername = findViewById(R.id.text_name);
+        mEmail = findViewById(R.id.text_mail);
+        mPassword = findViewById(R.id.text_password);
+        register = findViewById(R.id.btn_register);
+        toLogin = findViewById(R.id.toLogin);
     }
 }
