54a30fdf1b8ea0b031a1e7cd82ff2e6c4231f4fd
liu Wenxuan <woshitc8535@163.com>
2020-01-29 12:32:13 -0500

    Set up Layout of Login and Register

--- app/src/main/java/com/laioffer/usedbook/RegisterActivity.java
+++ app/src/main/java/com/laioffer/usedbook/RegisterActivity.java
@@ -1,22 +1,36 @@
 package com.laioffer.usedbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText mUsername;
     private EditText mEmail;
     private EditText mPassword;
-    private Button register;
+    private Button register_btn;
     private TextView toLogin;
 
     FirebaseAuth auth;
@@ -32,29 +46,86 @@ public class RegisterActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Register");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        register.setOnClickListener(new View.OnClickListener() {
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = mUsername.getText().toString();
                 String email = mEmail.getText().toString();
                 String password = mPassword.getText().toString();
+
+
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "All Field are needed", Toast.LENGTH_SHORT).show();
+                }
+                else if (password.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "The Password length has at least 6", Toast.LENGTH_SHORT);
+                }
+                else {
+                    register(username, email, password);
+                }
+            }
+        });
+
+
+        toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                finish();
             }
         });
     }
 
+    private void register(final String username, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            assert firebaseUser != null;
+                            String userid = firebaseUser.getUid();
 
 
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL", "default");
 
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(RegisterActivity.this, ControlPannel.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "YOu can't Register with this Email" , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
 
     private void init() {
         mUsername = findViewById(R.id.text_name);
         mEmail = findViewById(R.id.text_mail);
         mPassword = findViewById(R.id.text_password);
-        register = findViewById(R.id.btn_register);
+        register_btn = findViewById(R.id.btn_register);
         toLogin = findViewById(R.id.toLogin);
+
+        auth = FirebaseAuth.getInstance();
     }
 }
